26df62fec3550b0e287e24c51a15a8ecd81b4482
Jon Oliver <imjonoliver@gmail.com>
2016-05-11 01:06:37 -0400

    Add some drum variations

--- castlemania.rb
+++ castlemania.rb
@@ -1,15 +1,18 @@
 def play_random_scale notes, time
   play_pattern_timed notes.shuffle, time
 end
-
+play_loop = false
 fx = [:bitcrusher, :distortion, :flanger, :krush, :whammy, :wobble, :slicer]
 
 KEY = 'B'
 
+fx = :bitcrusher
+synth = :blade
+
 with_fx :reverb do
-  with_fx :bitcrusher do
+  with_fx fx do
     use_bpm 40
-    use_synth :blade
+    use_synth synth
     live_loop :piano1 do
       use_random_seed rand(1000)
       times = [0.5, 0.5, 0.5, 0.25]
@@ -19,20 +22,20 @@ with_fx :reverb do
       use_random_seed rand(1000)
       play_random_scale chord("#{KEY}1", :minor), 1
     end
-    live_loop :piano4 do
+    live_loop :piano3 do
       use_random_seed rand(1000)
       play_random_scale chord("#{KEY}2", :minor), 0.5
     end
   end
 end
 
+live_loop :rando, seed: 20 do
+  play_loop = one_in 2
+  sleep 4
+end
 live_loop :bd do
   sample :bd_boom
   sleep 4
-end
-live_loop :bd_808 do
-  sample :bd_haus
-  sleep 2
 end
 live_loop :snare do
   sleep 2
@@ -43,8 +46,31 @@ live_loop :snare do
   end
   sleep 2
 end
+live_loop :kick do
+  sample :bd_haus
+  sleep 2
+end
+live_loop :kick_fast do
+  16.times do
+    sample :bd_haus if play_loop && one_in(8)
+    sleep 0.125
+  end
+end
 live_loop :hat do
   sleep 0.5
   sample :drum_cymbal_closed
   sleep 0.5
+end
+live_loop :hat_fast do
+  16.times do
+    sleep 0.125
+    sample :drum_cymbal_closed if play_loop && one_in(2)
+  end
+end
+live_loop :open_hat do
+  with_fx :level, amp: 0.3 do
+    sleep 3.75
+    sample :drum_cymbal_open
+    sleep 0.25
+  end
 end
